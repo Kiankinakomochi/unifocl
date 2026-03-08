4b83e15726bed949736b54f2f1dc6fb6e9dc44fe
Kinichi Anju Makino <113649026+Kiankinakomochi@users.noreply.github.com>
2026-03-08 21:17:53 +0900

    Add hierarchy/inspector intellisense and bump to 0.3.1

--- src/unifocl/Services/CliVersion.cs
+++ src/unifocl/Services/CliVersion.cs
@@ -2,7 +2,7 @@ internal static class CliVersion
 {
     public const int Major = 0;
     public const int Minor = 3;
-    public const int Patch = 0;
+    public const int Patch = 1;
     public const string Protocol = "v2";
 
     public static string SemVer => $"{Major}.{Minor}.{Patch}";
