6e7a3c166fd2735ee0ce6b6d0a0f97bb8c7d2ad1
Kinichi Anju Makino <113649026+Kiankinakomochi@users.noreply.github.com>
2026-03-23 21:24:48 +0900

    close devcycle

--- src/unifocl/Services/CliVersion.cs
+++ src/unifocl/Services/CliVersion.cs
@@ -3,7 +3,7 @@ internal static class CliVersion
     public const int Major = 2;
     public const int Minor = 3;
     public const int Patch = 0;
-    public const string DevCycle = "a1";
+    public const string DevCycle = "";
     public const string Protocol = "v10";
 
     public static string SemVer => string.IsNullOrWhiteSpace(DevCycle)
